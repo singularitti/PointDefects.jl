5c4a4c31c3e0f33e822882871fd368d3b860bf1e
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-02-19 06:32:35 +0000

    build based on 4c81d1a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"index.html#","page":"Home","title":"Home","text":"CurrentModule = PointDefects","category":"page"},{"location":"index.html#PointDefects-1","page":"Home","title":"PointDefects","text":"","category":"section"},{"location":"index.html#","page":"Home","title":"Home","text":"","category":"page"},{"location":"index.html#","page":"Home","title":"Home","text":"Modules = [PointDefects]","category":"page"}]
+}
