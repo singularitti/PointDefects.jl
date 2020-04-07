e895a69bf252c389d77519138db59e094c190f52
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-04-07 01:11:17 +0000

    build based on 48286bf

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"index.html#","page":"Home","title":"Home","text":"CurrentModule = PointDefects","category":"page"},{"location":"index.html#PointDefects-1","page":"Home","title":"PointDefects","text":"","category":"section"},{"location":"index.html#","page":"Home","title":"Home","text":"","category":"page"},{"location":"index.html#","page":"Home","title":"Home","text":"Modules = [PointDefects]","category":"page"}]
+[{"location":"#","page":"Home","title":"Home","text":"CurrentModule = PointDefects","category":"page"},{"location":"#PointDefects-1","page":"Home","title":"PointDefects","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [PointDefects]","category":"page"}]
 }
